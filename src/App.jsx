c7ba84f603ceec7ca8482f15b5b850032b4c9be8
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-08-16 11:52:09 +0200

    added function for add color in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { initialColors } from "./lib/colors";
+import { useState } from "react";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/Color/Color-form";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  const addColor = (newColor) => {
+    const colorWithId = {
+      id: colors.length + 1,
+      ...newColor,
+    };
+    setColors([colorWithId, ...colors]);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
+      <ColorForm addColor={addColor} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
