dafaf423f623a8e19b375689ee5f212680e63861
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-08-19 10:19:13 +0200

    added handleInputChange for functionality in form results

---
+++ src/Components/Color/Color-input.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export default function ColorInput({ id, value, onChange }) {
+  const [inputValue, setInputValue] = useState(value);
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  function handleInputChange(event) {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  }
+
+  return (
+    <div className="color-input-container">
+      <input
+        type="text"
+        id={`${id}-text`}
+        name={id}
+        value={inputValue}
+        onChange={handleInputChange}
+        className="color-text-input"
+      />
+      <input
+        type="color"
+        id={`${id}-color`}
+        name={id}
+        value={inputValue}
+        onChange={handleInputChange}
+        className="color-picker"
+      />
+    </div>
+  );
+}
