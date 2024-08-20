ed7a78ede808f80afe6e1ea774046ca15b703828
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-08-20 11:12:21 +0200

    added solution for confirm delete

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,13 @@
 import "./Color.css";
+import "./Buttons.css";
 
-export default function Color({ color }) {
+export default function Color({
+  color,
+  onDelete,
+  confirmDelete,
+  onDeleteConfirm,
+  onCancel,
+}) {
   return (
     <div
       className="color-card"
@@ -12,6 +19,15 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {confirmDelete === color.id ? (
+        <div className="delete-container">
+          <p className="delete-question-area">Really delete?</p>
+          <button onClick={onCancel}>CANCEL</button>
+          <button onClick={onDeleteConfirm}>DELETE</button>
+        </div>
+      ) : (
+        <button onClick={() => onDelete(color.id)}>Delete</button>
+      )}
     </div>
   );
 }
